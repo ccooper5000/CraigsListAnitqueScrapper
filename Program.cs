fd5ea180000a98d4bf934cc9372dd8e368e1005e
IP Catch <ip.catch.ip@gmail.com>
2020-09-19 09:17:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace CraigsListAnitqueScrapper
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var html = "https://sanantonio.craigslist.org/search/ata";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html); 
+            var nodes = htmlDoc.DocumentNode.SelectNodes("//li[(@class= 'result-row') ]");
+            var rootNode = htmlDoc.DocumentNode;
+
+            foreach (var node in nodes)
+            {
+                Console.WriteLine(node.InnerText);
+            }
+        if (nodes == null)
+            {
+                Console.WriteLine("No items found.");
+            }
+
+        }
+    }
+}
